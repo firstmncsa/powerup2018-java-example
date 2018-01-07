90373ae2b345df96a17bed2aae8be09cebc66851
Team3840 <tkruse@deltamodtech.com>
2018-01-06 23:59:28 -0600

    Java Sample of single TalonSRX using CTRE v5.1.3.1 frameworks.

---
+++ SingleMotor/src/org/usfirst/frc3840/SingleMotor/RobotMap.java
@@ -0,0 +1,25 @@
+
+package org.usfirst.frc3840.SingleMotor;
+
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+public class RobotMap {
+   
+    public static TalonSRX runMotor1TalonSRX1;
+
+
+    public static void init() {
+    
+        runMotor1TalonSRX1 = new TalonSRX(1);
+   
+    }
+}
