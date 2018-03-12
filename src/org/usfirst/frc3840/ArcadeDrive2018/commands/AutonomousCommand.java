2e4072ec1addd506c668365e73593b5d09d533fc
tkruse <tkruse@deltamodtech.com>
2018-03-12 11:17:26 -0500

    Update AutonomousCommand.java
    Changed it to command group for example.

--- src/org/usfirst/frc3840/ArcadeDrive2018/commands/AutonomousCommand.java
+++ src/org/usfirst/frc3840/ArcadeDrive2018/commands/AutonomousCommand.java
@@ -1,42 +1,25 @@
 
 package org.usfirst.frc3840.ArcadeDrive2018.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *  Autonomous Command Code
+ * Autonomous Command code
  */
-public class AutonomousCommand extends Command {
+public class AutonomousCommand extends CommandGroup {
 
     public AutonomousCommand() {
-
- 
+    	// A command group will require all of the subsystems that each member would require.
+    	requires(Robot.driveTrain);   	
+    	
+        /**
+        * Example below to add a command group
+        */
+    	//drive to straight
+	  	//addParallel(new autoHoldCubeIn());
+	  	//addSequential(new LiftToTravel());
+	  	//addSequential(new autoDriveStraight());   
+    	
     }
 
-    // Called just before this Command runs the first time
-    @Override
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    @Override
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
-    }
 }
